f518fe63d63521a6dd9c2311aef7dc549d21c90f
cyal1 <example.qq.com>
2023-07-04 00:25:40 +0800

    update fetch url from file template

--- src/main/resources/examples/urls_from_file.py
+++ src/main/resources/examples/urls_from_file.py
@@ -1,13 +1,32 @@
+import sys
+
+# Run the command `python2 -c "import json; import sys; print json.dumps(sys.path)"` on your computer, and replace PIP_PATH below with the result.
+PIP_PATH = ["/Users/test/.pyenv/versions/2.7.18/lib/python2.7/site-packages"]
+
+for path in PIP_PATH:
+    if path not in sys.path:
+        sys.path.append(path)
+
+# pip install futures
+from concurrent.futures import ThreadPoolExecutor
+import threading
+
+
+def send_request(url, exit_event):
+    if exit_event.is_set():
+        return
+    sendRequest(makeRequest(url))
 
 
 def finish():
-    pool.shutdown()
+    exit_event.set()
 
 
-pool = RequestPool(20)
+executor = ThreadPoolExecutor(max_workers=10)
+exit_event = threading.Event()
 
-ip = "127.0.0.1"
+for url in open("/tmp/urls.txt"):
+    executor.submit(send_request, url, exit_event)
 
-for url in open('/tmp/urls.txt'):
-    pool.sendRequest(makeRequest(url).withHeader("X-Forwarded-For", ip))
+executor.shutdown(wait=False)
 
